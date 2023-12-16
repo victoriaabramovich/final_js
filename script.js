4a741467877351bb4afd6abf6b8eeddb71882d65
victoriaabramovich <abramovichvictoria@gmail.com>
2023-12-16 23:22:03 +0200

    add slider

--- script.js
+++ script.js
@@ -11,7 +11,54 @@ burgerElement.addEventListener('click', function() {
     // navigationElement.classList.toggle('navActive');
     ulElement.classList.toggle('activeNew');
 })
+// Slider
+let slideIndex = 1;
+/* Вызываем функцию, которая реализована ниже: */
+showSlides(slideIndex);
 
+/* Увеличиваем индекс на 1 — показываем следующий слайд: */
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
+function previousSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливаем текущий слайд: */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Функция перелистывания: */
+function showSlides(n) {
+    /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
+    let slides = document.getElementsByClassName("item");
+    
+    /* Проверяем количество слайдов: */
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+  
+    /* Проходим по каждому слайду в цикле for: */
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    /* Делаем элемент блочным: */
+    slides[slideIndex - 1].style.display = "block";    
+}
+
+
+
+
+
+
+
+//Accordion
 // let accordionContainers = document.querySelectorAll( ".accordion");
 //  for (let i = 0; i < accordionContainers.length; i++) {
 //     const element = accordionContainers[i];
